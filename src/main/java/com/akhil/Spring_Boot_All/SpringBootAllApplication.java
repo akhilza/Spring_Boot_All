84616081055162b21ba0018de7531a0ed33294b3
akhilza <akhil020398@gmail.com>
2022-11-11 11:33:35 -0500

    Initial commit

---
+++ src/main/java/com/akhil/Spring_Boot_All/SpringBootAllApplication.java
@@ -0,0 +1,13 @@
+package com.akhil.Spring_Boot_All;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akhil/Spring_Boot_All/SpringBootAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.Spring_Boot_All;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
